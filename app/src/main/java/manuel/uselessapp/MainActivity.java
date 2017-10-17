d7bb1a998f35f7350bfd02385cb2f78923851ec1
manuelkoliqi <Campeador123>
2017-10-17 16:11:54 +0200

    Added seekbar

--- app/src/main/java/manuel/uselessapp/MainActivity.java
+++ app/src/main/java/manuel/uselessapp/MainActivity.java
@@ -2,12 +2,39 @@ package manuel.uselessapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChangedValue = 0;
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChangedValue = progress;
+
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(MainActivity.this, "Progress is :" + progressChangedValue,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
